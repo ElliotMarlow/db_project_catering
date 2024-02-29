56e428d4fccd21d8ff0f6cd819b6347f0c9c0406
Dragon-Creator486 <efimova.2003@internet.ru>
2024-03-01 00:08:42 +0500

    1

--- ProjectDB/ProjectDB/Program.cs
+++ ProjectDB/ProjectDB/Program.cs
@@ -1,25 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => options.LoginPath = "/login");
+builder.Services.AddAuthorization();
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
 
+app.MapGet("/login", async (HttpContext context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    context.Response.SendFileAsync("html/login.html").GetAwaiter().GetResult();
+});
+app.MapPost("/login", async (string? returnUrl, HttpContext context) =>
+{
+    var form = context.Request.Form;
+    if (form.ContainsKey("login") && form.ContainsKey("password"))
+    {
+        string login = form["login"];
+        string password = form["password"];
+        if (true)
+        {
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, login) };
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+            context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity)).GetAwaiter().GetResult();
+            return Results.Redirect("/home");
+        }
+        else
+        {
+            return Results.Redirect("/login");
+        }
+    }
+    else { return Results.BadRequest("Email и/или пароль не установлены"); }
+});
+app.MapGet("/logout", (HttpContext context) =>
+{
+    context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return Results.Redirect("/login");
+});
+app.Map("/", (HttpContext context) => { context.Response.Redirect("/login"); });
 app.Run();
